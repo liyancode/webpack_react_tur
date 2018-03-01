f752a949db6f04e918e74b2a2900c850dc958214
liyancode <liyan.code@gmail.com>
2018-03-01 17:05:25 +0800

    nav btn click update

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ const temdata = [{
     "testid": "180117_082840_PKIG",
     "status": "error",
     "username": "admin666",
-    "api_content": "{\"API\":{\"ServerName_or_IP\":\"www.hellowd.com\",\"Http_or_Https\":\"http\",\"Method\":\"Post\",\"Path\":\"\",\"Parameters\":null,\"BodyValTypes\":\"form-data\",\"BodyData\":\"\"},\"Headers\":[[\"Accept\",\"application/json\"],[\"Authorization\",\"Bearer JYf0azPrf1RAvhUhpGZudVU9bBEa\"],[\"Accept-Language\",\"en-us\"]],\"ThreadProperties\":{\"Number_of_Threads\":\"1\",\"LoopCount\":\"3\"},\"SchedulerConfiguration\":{\"DurationSeconds\":\"300\"},\"UserDefinedVariables\":[],\"PerfmonSwitch\":\"false\",\"TargetHost\":\"\",\"FunctionTest\":\"true\"}",
+    "api_content": "{\"API\":{\"ServerName_or_IP\":\"www.hellowd.com/a/b/c?d=e\",\"Http_or_Https\":\"http\",\"Method\":\"Post\",\"Path\":\"\",\"Parameters\":null,\"BodyValTypes\":\"form-data\",\"BodyData\":\"\"},\"Headers\":[[\"Accept\",\"application/json\"],[\"Authorization\",\"Bearer JYf0azPrf1RAvhUhpGZudVU9bBEa\"],[\"Accept-Language\",\"en-us\"]],\"ThreadProperties\":{\"Number_of_Threads\":\"1\",\"LoopCount\":\"3\"},\"SchedulerConfiguration\":{\"DurationSeconds\":\"300\"},\"UserDefinedVariables\":[],\"PerfmonSwitch\":\"false\",\"TargetHost\":\"\",\"FunctionTest\":\"true\"}",
     "api_vuser_count": 1,
     "api_loop_count": 3,
     "api_target_host": "",
@@ -196,26 +196,29 @@ class App extends React.Component {
         this.state={
             currentPage:'tests',
             loginUser:'testuser',
-            editTest:{
-                valid:{
-                    vu:true,
-                    loop:true,
-                    url:false
+            editTest: {
+                valid: {
+                    vu: true,
+                    loop: true,
+                    url: false
                 },
-                load:{
-                    vu:1,
-                    loop:1,
-                    isFunctionTest:false
+                load: {
+                    vu: 1,
+                    loop: 1,
+                    isFunctionTest: false
                 },
-                apiInfo:{
-                    method:'get',
-                    httpOrHttps:'http',
-                    url:'',
-                    headers:[{hid:'0',k:'k1',v:'v1'},{hid:'1',k:'k2',v:'v2'}],
-                    body:''
-                }
+                apiInfo: {
+                    method: 'get',
+                    httpOrHttps: 'http',
+                    serverNameOrIP: '',
+                    urlPath: '',
+                    urlParams: '',
+                    url: '',
+                    headers: [{hid: '0', k: 'k1', v: 'v1'}, {hid: '1', k: 'k2', v: 'v2'}],
+                    body: ''
+                },
+                monitorHost: ''
             }
-            //{k:'k1',v:'v1'},{k:'k2',v:'v2'},{k:'k3',v:'v3'}
         };
         this.handleNavBtnClick=this.handleNavBtnClick.bind(this);
         this.handleProjectSelectChange=this.handleProjectSelectChange.bind(this);
@@ -230,6 +233,18 @@ class App extends React.Component {
         this.handleTestEditClick=this.handleTestEditClick.bind(this);
     }
     handleNavBtnClick(e){
+        // update tests list data
+        const page=e.target.getAttribute("data-nav_type");// "tests" or "reports"
+        if(page==='tests'){
+            $.ajax({
+                method:'get',
+                url:'',
+                headers:{X_RESTJMETER_TOKEN:'UkVTVEptZXRlcl9UT0tFTg=='},
+                success:function (resp) {
+                    console.log(resp);
+                }
+            });
+        }
         this.setState({
             currentPage:e.target.getAttribute("data-nav_type")
         });
@@ -254,7 +269,7 @@ class App extends React.Component {
         //     "testid": "1801110133_HB_RW",
         //     "status": "success",
         //     "username": "admin666",
-        //     "api_content": "{\"API\":{\"ServerName_or_IP\":\"www.hellowd.com\",\"Http_or_Https\":\"http\",\"Method\":\"GET\",\"Path\":\"\",\"Parameters\":null,\"BodyValTypes\":\"form-data\",\"BodyData\":\"\"},\"Headers\":[],\"ThreadProperties\":{\"Number_of_Threads\":\"1\",\"LoopCount\":\"10\"},\"SchedulerConfiguration\":{\"DurationSeconds\":\"300\"},\"UserDefinedVariables\":[],\"PerfmonSwitch\":\"false\",\"TargetHost\":\"\",\"FunctionTest\":\"false\"}",
+        //     "api_content": "{\"API\":{\"ServerName_or_IP\":\"www.hellowd.com/a/b/c?d=e\",\"Http_or_Https\":\"http\",\"Method\":\"GET\",\"Path\":\"\",\"Parameters\":null,\"BodyValTypes\":\"form-data\",\"BodyData\":\"\"},\"Headers\":[],\"ThreadProperties\":{\"Number_of_Threads\":\"1\",\"LoopCount\":\"10\"},\"SchedulerConfiguration\":{\"DurationSeconds\":\"300\"},\"UserDefinedVariables\":[],\"PerfmonSwitch\":\"false\",\"TargetHost\":\"\",\"FunctionTest\":\"false\"}",
         //     "api_vuser_count": 1,
         //     "api_loop_count": 10,
         //     "api_target_host": "",
@@ -277,8 +292,15 @@ class App extends React.Component {
         //api info
         const api=apiContent["API"];
         editTest.apiInfo.method=api["Method"].toLowerCase();
-        editTest.apiInfo.url=api["Http_or_Https"].toLowerCase()+'://'+api["ServerName_or_IP"]+api["Path"]+api["Parameters"];
+        editTest.apiInfo.serverNameOrIP=api["ServerName_or_IP"];
+        editTest.apiInfo.urlPath=api["Path"];
+        const tmpParam=(api["Parameters"]==='null'||api["Parameters"]==null)?'':api["Parameters"];
+        editTest.apiInfo.urlParams=tmpParam;
+        editTest.apiInfo.url=api["Http_or_Https"].toLowerCase()+'://'+api["ServerName_or_IP"]+api["Path"]+tmpParam;
         editTest.apiInfo.httpOrHttps=api["Http_or_Https"].toLowerCase();
+
+        //monitor host
+        editTest.monitorHost=apiContent["TargetHost"];
 
         //headers
         const apiHeaders=apiContent["Headers"];
@@ -320,8 +342,17 @@ class App extends React.Component {
         }else if(dataInput==='url'){
             valid.url=tmpValid;
             editTest.apiInfo.url=tmpValue;
-            editTest.apiInfo.httpOrHttps=tmpValue.split('://')[0]
-        }else{
+            const tmpSplitUrl=tmpValue.split('://');
+            editTest.apiInfo.httpOrHttps=tmpSplitUrl[0]
+            const tmpSplitUrl1=tmpSplitUrl[1].split(/\/(.+)/);
+            editTest.apiInfo.serverNameOrIP=tmpSplitUrl1[0];
+            const tmpSplitUrl2=tmpSplitUrl1[1].split('?');
+            editTest.apiInfo.urlPath=tmpSplitUrl2[0];
+            editTest.apiInfo.urlParams='?'+tmpSplitUrl2[1];
+        }else if(dataInput==='monitorHost'){
+            editTest.monitorHost=tmpValue;
+        }
+        else{
             //error
         }
         editTest.valid=valid;
@@ -420,6 +451,7 @@ class App extends React.Component {
             }
         );
     }
+
     // body text area onchange
     handleEditTestBodyTextareaOnChange(e){
         const target=e.target;
@@ -432,23 +464,58 @@ class App extends React.Component {
             }
         );
     }
+
     // submit test button click
     handleEditTestSubmitBtnClick(){
+        const apiToTest=this.state.editTest;
+
+        const apiToTestHeaders=apiToTest.apiInfo.headers;
+        let tmpHeaders=[];
+        for(let i=0;i<apiToTestHeaders.length;i++){
+            tmpHeaders.push([apiToTestHeaders[i]['k'],apiToTestHeaders[i]['v']]);
+        }
+        const data={
+            API:{
+                ServerName_or_IP:apiToTest.apiInfo.serverNameOrIP,
+                Http_or_Https:apiToTest.apiInfo.httpOrHttps,
+                Method:apiToTest.apiInfo.method,
+                Path:apiToTest.apiInfo.urlPath,
+                Parameters:apiToTest.apiInfo.urlParams,
+                BodyValTypes:'',
+                BodyData:apiToTest.apiInfo.body
+            },
+            Headers:tmpHeaders,//parse sth
+            ThreadProperties:{
+                Number_of_Threads:apiToTest.load.vu,
+                LoopCount:apiToTest.load.loop
+            },
+            SchedulerConfiguration:{
+                DurationSeconds:"300"
+            },
+            UserDefinedVariables:[],
+            PerfmonSwitch:apiToTest.monitorHost.length!=0,
+            TargetHost:apiToTest.monitorHost,
+            FunctionTest:apiToTest.load.isFunctionTest
+        }
         console.log(this.state);
         const valid=this.state.editTest.valid;
         if(valid.loop&&valid.url&&valid.vu){
-            alert("good");
+            // post the api test info
+            $.ajax({
+                url:'/rest/jmx',
+                method:'post',
+                headers:{X_RESTJMETER_TOKEN:'UkVTVEptZXRlcl9UT0tFTg=='},
+                data:JSON.stringify(data),
+                contentType: 'application/json; charset=utf-8',
+                success:function (resp) {
+                    console.log(resp);
+                }
+            });
         }else{
-            alert("false");
+            alert("sth error");
         }
     }
 
-    //componentWillUpdate(nextProps, nextState) {
-    //    $('#tests_table').DataTable().clear().draw();
-    //}
-    //componentDidMount(){
-    //    $('#tests_table').DataTable().columns.adjust().draw();
-    //}
     componentDidUpdate(){
         $('#tests_table').DataTable();
     }
